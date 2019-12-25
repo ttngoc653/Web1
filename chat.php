2701b13c4245651d750f63959783c118adbc37c2
1760081 <1760081@student.hcmus.edu.vn>
2019-12-25 23:47:17 +0700

    đang viết  giao diện chat và thêm hàm gửi mail theo định đạng

---
+++ chat.php
@@ -0,0 +1,19 @@
+<?php 
+include 'header.php';
+?>
+<div class="container-fluid row">
+  <div class="col-4" style="padding-right: 0px; padding-left: 0px;">
+   <div class="list-group" id="list-tab" role="tablist">
+    <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+    <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+    <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+    <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+  </div>
+</div>
+<div class="col-8">
+
+</div>
+</div>
+<?php 
+include 'footer.php';
+?>
